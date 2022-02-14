f10de4029a92cbf61931da12bdb48683b349eb5b
Yazmin Perez <zoeperez333@gmail.com>
2022-02-14 14:27:02 -0800

    fix quotation error

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,32 @@ function beepBoop(userNum) {
   let roboText = [];
   for (let i = 0; i <= userNum; i++) {
     let userStr = i.toString();
-    if (userStr.includes('3')) {
+    if (userStr.includes("3")) {
       roboText.push(" Won't you be my neighbor?");
-    } else if (userStr.includes('2')) {
+    } else if (userStr.includes("2")) {
       roboText.push(" Boop");
-    } else if (userStr.includes('1')) {
+    } else if (userStr.includes("1")) {
       roboText.push(" Beep");
     } else {
       roboText.push(userStr);
     }
   }
-  return roboText.join(', ');
+  return roboText.join(", ");
 }
 // user logic
 $(document).ready(function() {
-  $('form#neighbor').submit(function(event) {
+  $("form#neighbor").submit(function(event) {
     event.preventDefault();
-    let userInput = $('input#userInput').val();
+    let userInput = $("input#userInput").val();
     let result = beepBoop(userInput);
-    let fullName1Input = $('input#fullName').val();
-    $('.fullName').text(fullName1Input);
+    let fullName1Input = $("input#fullName").val();
+    $(".fullName").text(fullName1Input);
     $("#results").text(result);
     $("#resultsDisplay").show();
     
-    $('#reset').click(function() {
+    $("#reset").click(function() {
       $("#resultsDisplay").toggle().hide();
-      document.getElementById('neighbor').reset();
+      document.getElementById("neighbor").reset();
     });
   });
 });
